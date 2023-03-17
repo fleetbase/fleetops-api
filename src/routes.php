5eeee6a13df8b489fdf0822e0e552d71dd883af2
Ronald A. Richardson <me@ron.dev>
2023-03-17 16:05:46 +0800

    update routes

--- src/routes.php
+++ src/routes.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix(InternalConfig::get('api.routing.prefix', 'flb'))->namespace('Fleetbase\Http\Controllers')->group(
+Route::prefix(InternalConfig::get('api.routing.prefix', 'fleet-ops'))->namespace('Fleetbase\Http\Controllers')->group(
     function ($router) {
         /*
         |--------------------------------------------------------------------------
