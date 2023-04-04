f8e0f6a3e717623b5a5656629f733d259b9eadd7
Ronald A. Richardson <me@ron.dev>
2023-04-04 14:00:42 +0800

    added filters for `Vehicle` resources

--- src/Http/Filter/VehicleFilter.php
+++ src/Http/Filter/VehicleFilter.php
@@ -8,4 +8,34 @@ class VehicleFilter extends Filter
     {
         $this->builder->where('company_uuid', $this->session->get('company'));
     }
+
+    public function query(?string $query)
+    {
+        $this->builder->search($query);
+    }
+
+    public function vin(?string $vin)
+    {
+        $this->builder->searchWhere('vin', $vin);
+    }
+
+    public function internalId(?string $internalId)
+    {
+        $this->builder->searchWhere('internal_id', $internalId);
+    }
+
+    public function plateNumber(?string $plateNumber)
+    {
+        $this->builder->searchWhere('plate_number', $plateNumber);
+    }
+
+    public function vehicleMake(?string $make)
+    {
+        $this->builder->searchWhere('make', $make);
+    }
+
+    public function vehicleModel(?string $model)
+    {
+        $this->builder->searchWhere('model', $model);
+    }
 }
