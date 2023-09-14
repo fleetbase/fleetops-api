c2f20d6331a3d3a02cf3c8a0d3f3546ed723cd92
charleyfleetbase <charley@fleetbase.io>
2023-09-14 16:37:36 +0700

    Update vehicles and create vehicle devices table

---
+++ src/Models/VehicleDevices.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Fleetbase\FleetOps\Models;
+
+use Fleetbase\Models\Model;
+use Fleetbase\Traits\HasApiModelBehavior;
+use Fleetbase\Traits\HasUuid;
+use Fleetbase\Traits\HasPublicId;
+use Fleetbase\Traits\SendsWebhooks;
+use Fleetbase\Traits\TracksApiCredential;
+use Fleetbase\FleetOps\Casts\Polygon;
+use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
+
+class VehicleDevices extends Model
+{
+    use HasUuid,
+        HasPublicId,
+        SendsWebhooks,
+        TracksApiCredential,
+        SpatialTrait,
+        HasApiModelBehavior;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'vehicle_devices';
+
+    /**
+     * The type of public Id to generate
+     *
+     * @var string
+     */
+    protected $publicIdType = 'vehicle';
+
+    /**
+     * Attributes that is filterable on this model
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'vehicle_uuid',
+        'device_type',
+        'device_name',
+        'device_model',
+        'manufacturer',
+        'serial_number',
+        'installation_date',
+        'last_maintenance_date',
+        'meta',
+        'status',
+        'data_frequency',
+        'notes',
+    ];
+
+    /**
+     * Dynamic attributes that are appended to object
+     *
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * Filterable params.
+     *
+     * @var array
+     */
+    protected $filterParams = [];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Service area for rate.
+     *
+     * @var Model
+     */
+    public function serviceArea()
+    {
+        return $this->belongsTo(ServiceArea::class);
+    }
+
+    public function getTypeAttribute()
+    {
+        return 'zone';
+    }
+}
